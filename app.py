feec680445c3cc82095d4420d70ff56a0c90d5d6
Nan2006-ditha <marakanandhitha@gmail.com>
2026-05-28 21:57:14 +0530

    Fixed FastAPI routes

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-import json
-import os
-from datetime import datetime
 
 app = FastAPI()
 
@@ -16,28 +13,8 @@ def home():
 
 
 @app.get("/status")
-def get_status():
+def status():
 
-    if not os.path.exists("output.json"):
-
-        return {
-            "error": "output.json file not found",
-            "face_present": False
-        }
-
-    try:
-
-        with open("output.json", "r") as file:
-            data = json.load(file)
-
-        return {
-            "face_present": data.get("face_present", False),
-            "timestamp": data.get("timestamp", str(datetime.now()))
-        }
-
-    except Exception as e:
-
-        return {
-            "error": str(e),
-            "face_present": False
-        }
+    return {
+        "face_present": True
+    }
